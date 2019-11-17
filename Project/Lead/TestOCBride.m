f63350877aefb9183c14cc15442ab087ac30411a
XMG <kluthmao@163.com>
2019-11-18 02:26:17 +0800

    初始化项目

---
+++ Project/Lead/TestOCBride.m
@@ -0,0 +1,32 @@
+//
+//  TestOCBride.m
+//  Lead
+//
+//  Created by Kluth on 2019/11/18.
+//  Copyright © 2019 lorenzotech. All rights reserved.
+//
+
+#import "TestOCBride.h"
+
+@interface TestOCBride ()
+
+@end
+
+@implementation TestOCBride
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
